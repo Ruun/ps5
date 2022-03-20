acd8a8d839a422d16a5b07e59f205866a573d7a8
Ruan S <76751124+lelion1@users.noreply.github.com>
2022-03-19 20:26:38 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var menulist = document.getElementById("menulist");
+    menulist.style.maxHeight="0px";
+    function togglemenu(){
+        if(menulist.style.maxHeight=="0px")
+        {
+            menulist.style.maxHeight="130px";
+        }
+        else
+        {
+        menulist.style.maxHeight="0px";
+    }
+    }
